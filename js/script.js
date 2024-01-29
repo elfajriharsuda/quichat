077f214ef22cb51dd71e42ba9818018333575b8b
Elfajri Harsuda <elfajri.harsuda@gmail.com>
2024-01-30 01:23:35 +0700

    update validation

--- js/script.js
+++ js/script.js
@@ -2,36 +2,11 @@
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
 
 //validation number
-function setInputFilter(textbox, inputFilter, errMsg) {
-    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout"].forEach(function(event) {
-      textbox.addEventListener(event, function(e) {
-        if (inputFilter(this.value)) {
-          // Accepted value
-          if (["keydown","mousedown","focusout"].indexOf(e.type) >= 0){
-            this.classList.remove("input-error");
-            this.setCustomValidity("");
-          }
-          this.oldValue = this.value;
-          this.oldSelectionStart = this.selectionStart;
-          this.oldSelectionEnd = this.selectionEnd;
-        } else if (this.hasOwnProperty("oldValue")) {
-          // Rejected value - restore the previous one
-          this.classList.add("input-error");
-          this.setCustomValidity(errMsg);
-          this.reportValidity();
-          this.value = this.oldValue;
-          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        } else {
-          // Rejected value - nothing to restore
-          this.value = "";
-        }
-      });
-    });
-  }
+const inputPhone = document.querySelector("input[name='phone']");
 
-setInputFilter(document.getElementById("phone"), function(value) {
-    return /^-?\d*$/.test(value); }, "Must be number");
-
+inputPhone.addEventListener("input", function() {
+  this.value = this.value.replace(/\D/g, '');
+});
 
 //send to WA
 function sendMessage() {
